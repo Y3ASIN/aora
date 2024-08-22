b2135f7a873a9641864ca45ed20020c9a2b2a406
Yeasin Arafat <yeasinarafat@Yeasins-Air.local>
2024-08-22 23:46:21 +0600

    Add: Trending Video Section.

--- components/VideoCard.jsx
+++ components/VideoCard.jsx
@@ -1,5 +1,7 @@
 import { View, Text, Image, TouchableOpacity } from "react-native";
 import React, { useState } from "react";
+import { ResizeMode, Video } from "expo-av";
+
 import { icons } from "../constants";
 
 const VideoCard = ({
@@ -43,7 +45,18 @@ const VideoCard = ({
       </View>
 
       {play ? (
-        <Text className='text-white'>Playing</Text>
+        <Video
+          source={{ uri: video }}
+          className="w-full h-60 rounded-xl mt-3"
+          resizeMode={ResizeMode.CONTAIN}
+          useNativeControls
+          shouldPlay
+          onPlaybackStatusUpdate={(status) => {
+            if (status.didJustFinish) {
+              setPlay(false);
+            }
+          }}
+        />
       ) : (
         <TouchableOpacity
           activeOpacity={0.7}
