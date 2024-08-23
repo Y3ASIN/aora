258c4b2480e6c961b3f3115e922c2087c62fbb6a
Yeasin Arafat <yeasinarafat@Yeasins-Air.local>
2024-08-23 20:26:07 +0600

    Added: Profile Screen with all functionality

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -16,8 +16,10 @@ import EmptyState from "../../components/EmptyState";
 import { getAllPosts, getLatestPosts } from "../../lib/appwrite";
 import useAppwrite from "../../lib/useAppwrite";
 import VideoCard from "../../components/VideoCard";
+import { useGlobalContext } from "../../context/GlobalProvider";
 
 const Home = () => {
+  const { user, setUser, setIsLoggedIn } = useGlobalContext();
   const [refreshing, setRefreshing] = useState(false);
   
   const { data: posts, refetch } = useAppwrite(getAllPosts);
@@ -43,10 +45,10 @@ const Home = () => {
             <View className="justify-between items-start flex-row mb-6">
               <View>
                 <Text className="font-pmedium text-gray-100 text-sm">
-                  Welcome Back
+                  Welcome back
                 </Text>
                 <Text className="text-2xl font-psemibold text-white">
-                  Web Alchemist
+                  {user?.username}
                 </Text>
               </View>
 
